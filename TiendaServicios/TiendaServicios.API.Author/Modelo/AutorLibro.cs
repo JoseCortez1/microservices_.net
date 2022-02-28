3d53edc6489684fb54b03c97fc8dbe4c451bd3ce
Jose Cortez <jvazquez@nuxiba.com>
2022-02-28 17:58:05 -0600

    [ADD] microservices

---
+++ TiendaServicios/TiendaServicios.API.Author/Modelo/AutorLibro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaServicios.API.Author.Modelo
+{
+    public class AutorLibro
+    {
+        public int AutorLibroId { get; set; }
+        public string  Nombre { get; set;}
+        public string Apellido { get; set; }
+
+        public DateTime? FechaNacimiento { get; set; }
+
+        public ICollection<GradoAcademico> ListaGradosAcademicos { get; set; }
+
+        public string AutorLibroGuid { get; set; }
+
+    }
+}
